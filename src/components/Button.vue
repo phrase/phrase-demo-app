8315e2d0a0618c23cacc59f79ad26aa06c3a338d
Franca Winter <franca.winter@phrase.com>
2021-07-30 17:16:03 +0200

    Add button component and integrate in Hero

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <a href="">
+      <span>{{ $t('button_label') }}</span>
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Button',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<i18n>
+{
+  "en": {
+    "button_label": ""
+  }
+}
+</i18n>
+
+<style lang="scss" src="../assets/stylesheets/components/hero.scss"></style>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,12 +2,18 @@
   <section class="hero">
     <h2 class="text-xl">{{ $t('hero_title') }}</h2>
     <p class="text-m">{{ $t('hero_description') }}</p>
+    <Button />
   </section>
 </template>
 
 <script lang="ts">
+import Button from "./Button.vue";
+
 export default {
   name: 'Hero',
+  components: {
+    Button
+  },
   data() {
     return {}
   }
