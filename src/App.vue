a04dc00d67ff5d190ca7b382050c1882b3be2a58
Franca Winter <franca.winter@phrase.com>
2021-07-30 17:12:01 +0200

    Add Hero

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Nav />
+    <Hero />
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloI18n />
   </div>
@@ -9,11 +10,13 @@
 <script lang="ts">
 import HelloI18n from "./components/HelloI18n.vue";
 import Nav from "./components/Nav.vue";
+import Hero from "./components/Hero.vue";
 
 export default {
   name: 'App',
   components: {
     Nav,
+    Hero,
     HelloI18n
   }
 }
@@ -21,3 +24,4 @@ export default {
 
 <style lang="scss" src="./assets/stylesheets/shared/app.scss"></style>
 <style lang="scss" src="./assets/stylesheets/shared/colors.scss"></style>
+<style lang="scss" src="./assets/stylesheets/shared/typography.scss"></style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="hero">
+    <h2 class="text-xl">{{ $t('hero_title') }}</h2>
+    <p class="text-m">{{ $t('hero_description') }}</p>
+  </section>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Hero',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<i18n>
+{
+  "en": {
+    "hero_title": "",
+    "hero_description": ""
+  }
+}
+</i18n>
+
+<style lang="scss" src="../assets/stylesheets/components/hero.scss"></style>
