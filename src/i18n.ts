9b6efd215b4496009c23907496045805defc514f
Varpusparvi <mikko.karkee1@gmail.com>
2025-05-05 13:50:23 +0200

    CR

--- src/i18n.ts
+++ src/i18n.ts
@@ -13,25 +13,20 @@ export const i18nextInstance = i18next.createInstance({
 });
 
 export const initializeDemoI18next = async () => {
-  i18nextInstance.use({
-    type: 'backend',
-    read(language: string, namespace: string, callback: (errorValue: unknown, translations: unknown)=> void) {
-      // Path has to be relative to create chunks
-      import(`./locales/${language}.json`)
-        .then(resources => callback(null, resources))
-        .catch(error => callback(error, null));
-    },
-
-  }).use(new PhraseInContextEditorPostProcessor({
-    phraseEnabled: true,
-    projectId: '00000000000000004158e0858d2fa45c',
-    accountId: '0bed59e5',
-    useOldICE: false,
-  }));
-  await i18nextInstance.init();
+  await initializeI18next({
+    projectId: "00000000000000004158e0858d2fa45c",
+    accountId: "0bed59e5",
+  });
 };
 
 export const initializePrivateI18next = async () => {
+  await initializeI18next({
+    projectId: "aade46a6581311e26d892ef5a11df0a9",
+    accountId: "57a99e89e4ee5de2594afa2ab6cdc4c7",
+  });
+};
+
+const initializeI18next = async (settings: { projectId: string; accountId: string }) => {
   i18nextInstance
     .use({
       type: "backend",
@@ -45,8 +40,8 @@ export const initializePrivateI18next = async () => {
     .use(
       new PhraseInContextEditorPostProcessor({
         phraseEnabled: true,
-        projectId: "aade46a6581311e26d892ef5a11df0a9",
-        accountId: "57a99e89e4ee5de2594afa2ab6cdc4c7",
+        projectId: settings.projectId,
+        accountId: settings.accountId,
         useOldICE: false,
       })
     );
