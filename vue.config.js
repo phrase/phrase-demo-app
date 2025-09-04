9df10bbb8494d2c3d11bd405788e4677eb0eddab
Mikko Karkee <mikko.karkee1@gmail.com>
2025-09-04 20:48:52 +0200

    revert: "chore(SCM-717): Deploy to github pages (#38)" (#39)
    This reverts commit d8add31ca020896a26a96e6da93592d2e497a041.

--- vue.config.js
+++ vue.config.js
@@ -5,18 +5,16 @@ module.exports = {
         runtimeErrors: (error) => {
           if (error.message === "ResizeObserver loop limit exceeded") return false;
           return true;
-        },
-      },
-    },
+        }
+      }
+    }
   },
   pluginOptions: {
     i18n: {
-      locale: "en",
-      fallbackLocale: "en",
-      localeDir: "locales",
-      enableInSFC: false,
-    },
-  },
-  publicPath: process.env.NODE_ENV === "production" ? "/phrase-demo-app/" : "/",
-  outputDir: "dist",
-};
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
+}
