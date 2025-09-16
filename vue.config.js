5a5d77a37e48df8375a7e072d90a8f07d7a01520
Mikko Karkee <mikko.karkee1@gmail.com>
2025-09-16 15:41:36 +0200

    chore(SCM-717): Deploy to github pages (#40)
    * chore(SCM-717): Deploy to github pages (#39)
    
    * Adjust path
    
    * cleanup

--- vue.config.js
+++ vue.config.js
@@ -5,16 +5,18 @@ module.exports = {
         runtimeErrors: (error) => {
           if (error.message === "ResizeObserver loop limit exceeded") return false;
           return true;
-        }
-      }
-    }
+        },
+      },
+    },
   },
   pluginOptions: {
     i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
-      localeDir: 'locales',
-      enableInSFC: false
-    }
-  }
-}
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "locales",
+      enableInSFC: false,
+    },
+  },
+  publicPath: "/",
+  outputDir: "dist",
+};
